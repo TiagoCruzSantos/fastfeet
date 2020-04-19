802d06109c842e7a4781c727740fba4d00ac5661
Tiago da Cruz Santos <tiago.unity.cruz@gmail.com>
2020-04-18 21:27:59 -0300

    nome do recipiente q eu tinha esquecido

--- src/app/controllers/RecipientController.js
+++ src/app/controllers/RecipientController.js
@@ -4,18 +4,20 @@ const Yup = require('yup')
 class RecipientController {
     async store(req, res){
         const schema = Yup.object().shape({
+            name: Yup.string().required(),
             street: Yup.string().required(),
             number: Yup.number().required(),
             complement: Yup.string(),
             state: Yup.string().required(),
             city: Yup.string().required(),
-            cep: Yup.string().required()
+            cep: Yup.string().required().matches(/\d{5}-\d{3}/)
         })
         if(!(await schema.isValid(req.body))){
             return res.status(400).json({error: 'Validation failed'})
         }
-        const {id, street, number, complement, state, city, cep} = await Recipient.create(req.body)
+        const {name, id, street, number, complement, state, city, cep} = await Recipient.create(req.body)
         return res.json({
+            name,
             id,
             street,
             number,
--- src/app/models/Recipient.js
+++ src/app/models/Recipient.js
@@ -3,6 +3,7 @@ const Sequelize = require('sequelize')
 class Recipient extends Sequelize.Model{
     static init(sequelize){
         super.init({
+            name: Sequelize.STRING,
             street: Sequelize.STRING,
             number: Sequelize.INTEGER,
             complement: Sequelize.STRING,
